123eba598d7d8e80c556eff539d7e54a36e3d5e1
びくんびくん <big.stone.net@gmail.com>
2019-09-13 13:21:42 +0900

    add firebase event Trigger

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,15 @@
 const functions = require('firebase-functions');
 
-exports.hello = functions.https.onRequest((request, response) => {
+const REGION = 'asia-northeast1';
+
+exports.hello = functions.region(REGION).https.onRequest((request, response) => {
     response.status(200).send("Hello World")
-})
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+});
+
+exports.createDevice = functions.region(REGION)
+    .firestore
+    .document('devices/{deviceId}')
+    .onCreate((snap, context) => {
+        const newValue = snap.data();
+        const name = newValue.name;
+});
